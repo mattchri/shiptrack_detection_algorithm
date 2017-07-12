786dabf6de50cac17615cdf6f9e32c3e67b395c3
mattchri <matthew.christensen@stfc.ac.uk>
2017-07-12 14:22:07 +0100

    Create fetch_data.sh
    Use these lines to download the test data if git lfs is not installed on the system

---
+++ test_data/fetch_data.sh
@@ -0,0 +1,8 @@
+wget https://media.githubusercontent.com/media/mattchri/shiptrack_detection_algorithm/master/test_data/MOD021KM.A2002150.1855.006.2014231005506.hdf
+wget https://media.githubusercontent.com/media/mattchri/shiptrack_detection_algorithm/master/test_data/MOD03.A2002150.1855.006.2012254205122.hdf
+wget https://media.githubusercontent.com/media/mattchri/shiptrack_detection_algorithm/master/test_data/MOD04_L2.A2002150.1855.006.2014313005830.hdf
+wget https://media.githubusercontent.com/media/mattchri/shiptrack_detection_algorithm/master/test_data/MOD06_L2.A2002150.1855.006.2014313023238.hdf
+mv MOD021KM.A2002150.1855.006.2014231005506.hdf.1 MOD021KM.A2002150.1855.006.2014231005506.hdf
+mv MOD03.A2002150.1855.006.2012254205122.hdf.1 MOD03.A2002150.1855.006.2012254205122.hdf
+mv MOD04_L2.A2002150.1855.006.2014313005830.hdf.1 MOD04_L2.A2002150.1855.006.2014313005830.hdf
+mv MOD06_L2.A2002150.1855.006.2014313023238.hdf.1 MOD06_L2.A2002150.1855.006.2014313023238.hdf
